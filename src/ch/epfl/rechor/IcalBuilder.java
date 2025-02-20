58078344b68ed838cc096af4ca59c3bee2a8fd46
tom.beauge <tom.m.beauge@gmail.com>
2025-02-20 23:54:20 +0100

    ical add method implemented

---
+++ src/ch/epfl/rechor/IcalBuilder.java
@@ -0,0 +1,45 @@
+package ch.epfl.rechor;
+
+public final class IcalBuilder {
+    private final int MAX_LINE_LENGTH = 75;
+    private static final String CRLF = "\r\n";
+    private final StringBuilder builder = new StringBuilder();
+
+    public enum Component {
+        VCALENDAR,
+        VEVENT;
+    }
+
+    public enum Name {
+        BEGIN,
+        END,
+        PRODID,
+        VERSION,
+        UID, DTSTAMP,
+        DTSTART,
+        DTEND,
+        SUMMARY,
+        DESCRIPTION;
+    }
+
+    public IcalBuilder add(Name name, String value){
+        int nChar = 0;
+
+        StringBuilder text = new StringBuilder(name + ": " + value);
+        int textLength = text.length();
+
+
+        while (nChar < textLength) {
+
+            if (nChar % MAX_LINE_LENGTH == 0){
+                text.insert(nChar, CRLF + " ");
+                textLength += 3; //accounts for CRLF char count as well as space
+            }
+            nChar++;
+        }
+
+        builder.append(text).append(CRLF);
+
+        return this;
+    }
+}
