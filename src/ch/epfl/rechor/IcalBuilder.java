99679dfb53dd4cbd782bd84a6a3be3835ac74ee7
tom.beauge <tom.m.beauge@gmail.com>
2025-02-24 19:39:59 +0100

    finished icalBuilder

--- src/ch/epfl/rechor/IcalBuilder.java
+++ src/ch/epfl/rechor/IcalBuilder.java
@@ -1,11 +1,13 @@
 package ch.epfl.rechor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public final class IcalBuilder {
     private final int MAX_LINE_LENGTH = 75;
     private static final String CRLF = "\r\n";
-    private final StringBuilder builder = new StringBuilder();
+    private final StringBuilder sb = new StringBuilder();
+    private final ArrayList<Component> buildingComponents = new ArrayList<Component>();
 
     public enum Component {
         VCALENDAR,
@@ -40,26 +42,38 @@ public final class IcalBuilder {
             nChar++;
         }
 
-        builder.append(text).append(CRLF);
+        sb.append(text).append(CRLF);
 
         return this;
     }
 
-    //TODO
     public IcalBuilder add(Name name, LocalDateTime dateTime) {
-        return null;
+        String formattedDateTime = String.format("%04d%02d%02dT%02d%02d%02d",
+                dateTime.getYear(), dateTime.getMonthValue(), dateTime.getDayOfMonth(),
+                dateTime.getHour(), dateTime.getMinute(), dateTime.getSecond());
+        return add(name, formattedDateTime);
     }
 
     public IcalBuilder begin(Component component) {
-        return null;
+        buildingComponents.add(component);
+        return add(Name.BEGIN, component.name());
     }
 
     public IcalBuilder end() {
-        return null;
+        if (buildingComponents.isEmpty()) {
+            throw new IllegalStateException("There are no components to build");
+        }
+
+        //closes the most recent component and removes it from our list
+        Component component = buildingComponents.remove(buildingComponents.size() - 1);
+        return add(Name.END, component.name());
     }
 
     public String build() {
-        return null;
+        if (!buildingComponents.isEmpty()) {
+            throw new IllegalStateException("Some components have not been closed");
+        }
+        return sb.toString();
     }
 
 }
