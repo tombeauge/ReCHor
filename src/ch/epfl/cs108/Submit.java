790011640e8b571b58bf9d5a77a324bacd777d70
cem-c <cem.celik@epfl.ch>
2025-03-07 10:26:56 +0100

    Token for 3rd week put in

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -30,7 +30,7 @@ public final class Submit {
     // CONFIGURATION
     // -------------
     // Jeton du premier membre du groupe
-    private static final String TOKEN_1 = "EeFai3ke"; //Cem 2nd step
+    private static final String TOKEN_1 = "iC8PieFa"; //Cem 3rd step
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
     private static final String TOKEN_2 = "Ko1Ooqua"; //Tom 2nd step
     // Noms des éventuels fichiers Java additionnels à inclure (p.ex. "MyClass.java")
