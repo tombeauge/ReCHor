707269424fdb811bb7755a5174c0f873b9df169c
cem-c <cem.celik@epfl.ch>
2025-03-09 12:09:33 +0100

    3.4 - 3.5

---
+++ src/ch/epfl/rechor/timetable/mapped/BufferedPlatforms.java
@@ -0,0 +1,44 @@
+package ch.epfl.rechor.timetable.mapped;
+
+import ch.epfl.rechor.timetable.Platforms;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import static ch.epfl.rechor.timetable.mapped.Structure.field;
+
+public final class BufferedPlatforms implements Platforms {
+
+    List<String> stringTable;
+    ByteBuffer buffer;
+    int  NAME_ID  =  0 ;
+    int  STATION_ID  =  1;
+    StructuredBuffer platformStructureBuffer;
+    Structure platformStructure;
+
+    public BufferedPlatforms(List<String> stringTable, ByteBuffer buffer) {
+        this.stringTable = stringTable;
+        this.buffer = buffer;
+        platformStructure = new Structure(field(NAME_ID, Structure.FieldType.U16),
+                field(STATION_ID, Structure.FieldType.U16));
+        platformStructureBuffer = new StructuredBuffer(platformStructure, buffer);
+    }
+
+
+    @Override
+    public String name(int id) {
+        int platformNameID = platformStructureBuffer.getU16(NAME_ID,id);
+
+        return stringTable.get(platformNameID);
+    }
+
+    @Override
+    public int stationId(int id) {
+        return platformStructureBuffer.getU16(STATION_ID,id);
+    }
+
+    @Override
+    public int size() {
+        return platformStructureBuffer.size();
+    }
+}
---
+++ src/ch/epfl/rechor/timetable/mapped/BufferedStationAliases.java
@@ -0,0 +1,49 @@
+package ch.epfl.rechor.timetable.mapped;
+
+import ch.epfl.rechor.timetable.StationAliases;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import static ch.epfl.rechor.timetable.mapped.Structure.field;
+
+public final class BufferedStationAliases implements StationAliases {
+
+    List<String> stringTable;
+    ByteBuffer buffer;
+    int  ALIAS_ID  =  0 ;
+    int  STATION_NAME_ID  =  1;
+    StructuredBuffer stationAliasesStructureBuffer;
+    Structure stationAliasesStructure;
+
+
+
+    BufferedStationAliases(List<String> stringTable, ByteBuffer buffer) {
+        this.stringTable = stringTable;
+        this.buffer = buffer;
+        stationAliasesStructure = new Structure(field(ALIAS_ID, Structure.FieldType.U16),
+                field(STATION_NAME_ID, Structure.FieldType.U16));
+        stationAliasesStructureBuffer = new StructuredBuffer(stationAliasesStructure, buffer);
+
+    }
+
+
+    @Override
+    public String alias(int id) {
+        int aliasID = stationAliasesStructureBuffer.getU16(ALIAS_ID,id);
+
+        return stringTable.get(aliasID);
+    }
+
+    @Override
+    public String stationName(int id) {
+        int stationName = stationAliasesStructureBuffer.getU16(STATION_NAME_ID,id);
+
+        return stringTable.get(stationName);
+    }
+
+    @Override
+    public int size() {
+        return stationAliasesStructureBuffer.size();
+    }
+}
