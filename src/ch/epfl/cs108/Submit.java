fe0888e68b07231fedca26d5840c4a08d178bee6
Tom Beauge <tom.beauge@epfl.ch>
2025-03-21 13:32:07 +0100

    tom 5th step token
    Took 4 minutes

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -32,7 +32,7 @@ public final class Submit {
     // Jeton du premier membre du groupe
     private static final String TOKEN_1 = "iu2AiGi4"; //Cem 5th step
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "usooPab9"; //Tom 4th step
+    private static final String TOKEN_2 = "Too9ae8o"; //Tom 5th step
     // Noms des éventuels fichiers Java additionnels à inclure (p.ex. "MyClass.java")
     private static final List<String> ADDITIONAL_FILES =
             List.of();
